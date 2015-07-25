ae4397903e5a775040887809396eb45c7fcfe690
Andrew Ito <andrew_ito@datahouse.com>
2015-07-24 23:17:20 -1000

    adding exapmle code

--- js/hangman.js
+++ js/hangman.js
@@ -1,5 +1,30 @@
+/* --------------------------------------------- *\
+ * initialize script
+\* --------------------------------------------- */
 function logger(msg){
 	console.log(msg);
 }
 
 logger("hangman code loaded");
+
+/* -------------------------------------------- *\
+ * listeners
+\* -------------------------------------------- */
+$('#firstButton').click(function(){
+	var userInput = getInput();
+	setOutput(userInput);
+});
+
+/* -------------------------------------------- *\
+ * functions
+\* -------------------------------------------- */
+function getInput(){
+	return $('#firstInput').val();
+}
+
+function setOutput(message){
+	if(message == false){
+		message = 'my output';
+	}
+	$('#textOutput').html('set output says:'+message);
+}
