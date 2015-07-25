0672bf6e4cb06ec7fdc7dcd70b2e12a6b39fbefb
Andrew Ito <andrew_ito@datahouse.com>
2015-07-25 07:22:15 -1000

    adding server parts

--- js/network.js
+++ js/network.js
@@ -1,1 +1,30 @@
 logger("network manager loaded");
+
+/* ************************************************************* *\
+ * init
+\* ************************************************************* */
+requestWordFindData(new Array(3));
+/* ************************************************************* *\
+ * listeners
+\* ************************************************************* */
+
+/* ************************************************************* *\
+ * functions
+\* ************************************************************* */
+function requestWordFindData(wordArray){
+	wordArray = _.map(wordArray, function(word){
+		return word || '?'
+	});
+	
+	var reqOptions = {
+		method:'post',
+		url:'server.php',
+		data:{numberOfLetters:wordArray.length, word:wordArray.join('')}
+	}
+	logger(reqOptions);
+
+	$.ajax(reqOptions).done(function(msg){
+		console.log(msg);
+		console.log('done');
+	});
+}
