4a52f61af327e900ae1dc06723d48795a9d8b87e
andrew_ito <andrew_ito@datahouse.com>
2015-07-27 23:54:15 -1000

    network.js additions to process html and find unique unguessed words

--- js/network.js
+++ js/network.js
@@ -3,7 +3,17 @@ logger("network manager loaded");
 /* ************************************************************* *\
  * init
 \* ************************************************************* */
-requestWordFindData(new Array(3));
+
+function exampleTurn() {
+  var currentGuesses = ['a', 'e', 'o', 'u'];
+  requestWordFindData(new Array(3), function (rawHtml) {
+    var wordList = getWordListFromHtml(rawHtml);
+    wordList = wordsLessGuessedLetters(currentGuesses, wordList);
+
+    console.log(wordList);
+  });
+}
+
 /* ************************************************************* *\
  * listeners
 \* ************************************************************* */
@@ -11,7 +21,7 @@ requestWordFindData(new Array(3));
 /* ************************************************************* *\
  * functions
 \* ************************************************************* */
-function requestWordFindData(wordArray){
+function requestWordFindData(wordArray, callback){
 	wordArray = _.map(wordArray, function(word){
 		return word || '?'
 	});
@@ -20,11 +30,42 @@ function requestWordFindData(wordArray){
 		method:'post',
 		url:'server.php',
 		data:{numberOfLetters:wordArray.length, word:wordArray.join('')}
-	}
+	};
+
 	logger(reqOptions);
 
 	$.ajax(reqOptions).done(function(msg){
-		console.log(msg);
-		console.log('done');
+    logger('grabbed raw html');
+    callback(msg);
 	});
 }
+
+function getWordListFromHtml(rawHtml){
+  var wordList = [];
+  var structured = rawHtml.split('href="/word/');
+
+  //skip the first entry cause it has loads of gibberish.
+  structured.shift();
+
+  //loop through structured data, cut off pieces, and add it to the wordlist.
+  _.each(structured, function(line){
+    var endOfLine = line.indexOf('/"');
+    if(endOfLine >= 0) {
+      wordList.push(line.substring(0,endOfLine));
+    }
+  });
+
+  return (wordList);
+}
+
+function wordsLessGuessedLetters(guesses, wordList){
+  var unguessed = [];
+  var regex = new RegExp('[' + guesses.join('') + ']');
+
+  _.each(wordList, function (word) {
+    if(!word.match(regex)){
+      unguessed.push(word);
+    };
+  });
+  return unguessed;
+};
